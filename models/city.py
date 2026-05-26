566a50ac8b378227c6fc2266315784c8ee6ed953
Basant Morsi <basant179@gmail.com>
2026-05-26 11:55:55 +0200

    Modify relationship between city and country in model

--- models/city.py
+++ models/city.py
@@ -1,6 +1,8 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
+
 
 class City(SQLModel, table= True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     country_id: int = Field(foreign_key="country.id")
+    country: "Country" = Relationship(back_populates="cities")
--- models/country.py
+++ models/country.py
@@ -1,6 +1,7 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 class Country(SQLModel, table= True):
     id: int |None = Field(default=None, primary_key=True)
     # database create index on the name to search faster
     name: str = Field(index=True)
+    cities: list["City"] = Relationship(back_populates="country")
