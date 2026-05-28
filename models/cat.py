6517f73ce3643dce42f28cb6e49f83cdc5b6b972
Basant Morsi <basant179@gmail.com>
2026-05-28 17:11:58 +0200

    Creating a model for cat

--- models/cat.py
+++ models/cat.py
@@ -1,9 +1,19 @@
 from sqlmodel import Field, SQLModel
 from enum import Enum
 
+class CatGender(str, Enum):
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+
 
 class Cat(SQLModel, table= True):
-    id:int | None = Field(default=None, primary_key=True)
-    name:str
-    age:int
+    id: int | None = Field(default=None, primary_key=True)
+    owner_id: int = Field(foreign_key="owner.id")
+    name: str
+    age: int
+    breed_id: int = Field(foreign_key="breed.id")
+    gender: CatGender
+    color: str
+    notes: str
+
 
