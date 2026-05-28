94ffacfaa2198d81210d4bbfd61384ad99658485
Basant Morsi <basant179@gmail.com>
2026-05-28 13:23:01 +0200

    Creating breed module

---
+++ models/breed.py
@@ -0,0 +1,12 @@
+import sqlmodel
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+
+class Breed(SQLModel, table = True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    category: str
+    hair_length: str | None = Field(default=None)
+    is_official_breed: bool = Field(default=True)
+    registries: str | None = Field(default=None)
